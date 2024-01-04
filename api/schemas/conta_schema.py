21bf3c43cfba11ca98a67c8ce48a3ded56bd4a05
Alan Pancieri Berger Saar <alan.berger@ufes.br>
2024-01-04 13:40:03 -0300

    ultimas modificações antes do frontend

--- api/schemas/conta_schema.py
+++ api/schemas/conta_schema.py
@@ -5,7 +5,7 @@ from ..schemas import operacao_schema
 
 
 class ContaSchema(marshmallow.SQLAlchemyAutoSchema):
-    operacoes = marshmallow.Nested(operacao_schema.OperacaoSchema, many=True, only=('id', 'nome', 'resumo', 'tipo', 'custo'))
+    operacoes = marshmallow.Nested(operacao_schema.OperacaoSchema, many=True, only=('id', 'nome', 'resumo', 'tipo', 'custo', 'data'))
     class Meta:
         model = conta_model.Conta
         load_instance = True
